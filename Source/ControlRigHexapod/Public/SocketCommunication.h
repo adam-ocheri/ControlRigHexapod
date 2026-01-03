9890e61a227d84cca5cf584e7ee9b2ae184b83db
Adam Ocheri <adamotchery@gmail.com>
2026-01-04 00:02:27 +0200

    init commit

---
+++ Source/ControlRigHexapod/Public/SocketCommunication.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SocketCommunication.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONTROLRIGHEXAPOD_API USocketCommunication : public UObject
+{
+	GENERATED_BODY()
+	
+};
